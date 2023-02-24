9944f909c31aead3410ad07dd8238c4177c57441
denitusha <denitusha3@gmail.com>
2023-02-24 19:38:26 +0100

    Inferring more

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -12,19 +12,8 @@ struct MemoryGame <CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     private(set) var score: Int = 0
     private var indexOfTheOneAndOnlyFaceUpCard: Int? {
-        get {
-            cards.indices.filter({ cards[$0].isFaceUp}).oneAndOnly
-        }
-        set {
-            for index in cards.indices {
-                if index != newValue {
-                    cards[index].isFaceUp = false
-                }else{
-                    cards[index].isFaceUp = true
-                }
-                
-            }
-        }
+        get {cards.indices.filter({ cards[$0].isFaceUp}).oneAndOnly}
+        set { cards.indices.forEach{cards[$0].isFaceUp = ($0 == newValue)}}
     }
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: {$0.id == card.id }),
